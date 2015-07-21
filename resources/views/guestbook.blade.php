3e99ac47bbef0177f02e444c3c67d335e89b7993
605527108 <605527108shif@gmail.com>
2015-07-21 17:12:22 +0800

    change cdn

--- resources/views/guestbook.blade.php
+++ resources/views/guestbook.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Words</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">    
     <style>
         body { margin: 2em 0; }
     </style>
--- resources/views/word/detail.blade.php
+++ resources/views/word/detail.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta id="token" name="token" value="{{ csrf_token() }}">
     <title>Words</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">    
     <style>
         body { margin: 2em 0; }
     </style>
